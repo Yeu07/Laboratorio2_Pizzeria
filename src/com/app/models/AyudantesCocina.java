db5ad143ac7bb6445a4c513bf84a0b00f14af765
Yeu07 <106332424+Yeu07@users.noreply.github.com>
2022-11-05 14:38:02 -0300

    Delete AyudantesCocina.java

--- src/com/app/models/AyudantesCocina.java
+++
@@ -1,9 +0,0 @@
-package com.app.models;
-
-import java.util.Date;
-
-public class AyudantesCocina extends Empleado {
-	public AyudantesCocina(String nombre, String nacionalidad, float sueldo, int dni, Date fechaNacimiento) {
-		super(nombre,nacionalidad,sueldo,dni,fechaNacimiento);
-	}
-}
